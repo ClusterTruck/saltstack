22b24f2df4a23ba88a7ba3b12c4248c92b89b4d8
Andrew Morin <amorin@clustertruck.com>
2019-09-25 13:49:31 -0400

    * Add Ingress support to Kubernetes Module * Add Kubernetes `ingress_present` and `ingress_absent` states

--- salt/states/kubernetes.py
+++ salt/states/kubernetes.py
@@ -1177,3 +1177,133 @@ def statefulset_present(
     }
     ret["result"] = True
     return ret
+
+
+def ingress_absent(name, namespace="default", **kwargs):
+    """
+    Ensures that the named ingress is absent from the given namespace.
+
+    name
+        The name of the ingress
+
+    namespace
+        The name of the namespace
+    """
+
+    ret = {"name": name, "changes": {}, "result": False, "comment": ""}
+
+    ingress = __salt__["kubernetes.show_ingress"](name, namespace, **kwargs)
+
+    if ingress is None:
+        ret["result"] = True if not __opts__["test"] else None
+        ret["comment"] = "The ingress does not exist"
+        return ret
+
+    if __opts__["test"]:
+        ret["comment"] = "The ingress is going to be deleted"
+        ret["result"] = None
+        return ret
+
+    res = __salt__["kubernetes.delete_ingress"](name, namespace, **kwargs)
+    if res["code"] == 200:
+        ret["result"] = True
+        ret["changes"] = {
+            "kubernetes.ingress": {
+                "new": "absent", "old": "present"}}
+        ret["comment"] = res["message"]
+    else:
+        ret["comment"] = "Something went wrong, response: {0}".format(res)
+
+    return ret
+
+
+def ingress_present(
+    name,
+    namespace="default",
+    metadata=None,
+    spec=None,
+    source="",
+    template="",
+    **kwargs):
+    """
+    Ensures that the named ingress is present inside of the specified
+    namespace with the given metadata and spec.
+    If the ingress exists it will be replaced.
+
+    name
+        The name of the ingress.
+
+    namespace
+        The namespace holding the ingress. The "default" one is going to be
+        used unless a different one is specified.
+
+    metadata
+        The metadata of the ingress object.
+
+    spec
+        The spec of the ingress object.
+
+    source
+        A file containing the definition of the ingress (metadata and
+        spec) in the official kubernetes format.
+
+    template
+        Template engine to be used to render the source file.
+    """
+    ret = {"name": name, "changes": {}, "result": False, "comment": ""}
+
+    if (metadata or spec) and source:
+        return _error(
+            ret,
+            "\"source\" cannot be used in combination with \"metadata\" or "
+            "\"spec\""
+        )
+
+    if metadata is None:
+        metadata = {}
+
+    if spec is None:
+        spec = {}
+
+    ingress = __salt__["kubernetes.show_ingress"](name, namespace, **kwargs)
+
+    if ingress is None:
+        if __opts__["test"]:
+            ret["result"] = None
+            ret["comment"] = "The ingress is going to be created"
+            return ret
+        res = __salt__["kubernetes.create_ingress"](name=name,
+                                                       namespace=namespace,
+                                                       metadata=metadata,
+                                                       spec=spec,
+                                                       source=source,
+                                                       template=template,
+                                                       saltenv=__env__,
+                                                       **kwargs)
+        ret["changes"]["{0}.{1}".format(namespace, name)] = {
+            "old": {},
+            "new": res}
+    else:
+        if __opts__["test"]:
+            ret["result"] = None
+            return ret
+
+        # TODO: improve checks  # pylint: disable=fixme
+        log.info("Forcing the recreation of the ingress")
+        ret["comment"] = "The ingress is already present. Forcing recreation"
+        res = __salt__["kubernetes.replace_ingress"](
+            name=name,
+            namespace=namespace,
+            metadata=metadata,
+            spec=spec,
+            source=source,
+            template=template,
+            saltenv=__env__,
+            **kwargs)
+
+    ret["changes"] = {
+        "metadata": metadata,
+        "spec": spec
+    }
+    ret["result"] = True
+    return ret
