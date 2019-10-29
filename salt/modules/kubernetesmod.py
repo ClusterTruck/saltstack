6b40d156d2c44f1c31069a52191ea1cecfca7908
Andrew Morin <amorin@clustertruck.com>
2019-10-29 13:44:20 -0400

    Fix base64 encoding of secrets for k8s API

--- salt/modules/kubernetesmod.py
+++ salt/modules/kubernetesmod.py
@@ -1473,7 +1473,7 @@ def create_secret(
 
     # encode the secrets using base64 as required by kubernetes
     for key in data:
-        data[key] = base64.b64encode(data[key])
+        data[key] = str(base64.b64encode(data[key].encode('utf-8')), 'utf-8')
 
     body = kubernetes.client.V1Secret(
         metadata=__dict_to_object_meta(name, namespace, {}),
@@ -1897,7 +1897,7 @@ def replace_secret(name,
 
     # encode the secrets using base64 as required by kubernetes
     for key in data:
-        data[key] = base64.b64encode(data[key])
+        data[key] = str(base64.b64encode(data[key].encode('utf-8')), 'utf-8')
 
     body = kubernetes.client.V1Secret(
         metadata=__dict_to_object_meta(name, namespace, {}),
